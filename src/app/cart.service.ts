906a290893543e3657c42daacee6c4dd9b99859c
shaileshbhosale <shailesh.b@saksoft.com>
2020-06-29 19:44:09 +0530

    Manage data: 3 files modified, 4 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+  }
+)
 export class CartService {
 
   items = [];
@@ -17,7 +21,13 @@ export class CartService {
     this.items = [];
     return this.items;
   }
-  
-  constructor() { }
+
+  getShippingPrices() {
+    return this.http.get('/assets/shipping.json');
+  }
+
+  constructor(
+    private http: HttpClient
+  ) { }
 
 }
